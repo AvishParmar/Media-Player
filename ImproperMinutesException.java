38924faa8ae58456c1dae4d024adaccea6c13faf
Avish Parmar <46851126+AvishParmar@users.noreply.github.com>
2020-09-16 09:27:22 -0400

    Update ImproperMinutesException.java

--- ImproperMinutesException.java
+++ ImproperMinutesException.java
@@ -1,10 +1,6 @@
-package Homework1;
 /**
  * ImproperMinutesException class that is invoked when an ImproperMinutesException is thrown.
  * @author Avish Parmar
- * SBUID: 112647892
- * Email: avish.parmar@stonybrook.edu
- * Course: CSE214
  */
 public class ImproperMinutesException extends Exception {
 	
