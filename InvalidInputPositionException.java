c00ca603b99578e5a6fe9b52493b28638eeb1402
Avish Parmar <46851126+AvishParmar@users.noreply.github.com>
2020-09-16 09:28:14 -0400

    Update InvalidInputPositionException.java

--- InvalidInputPositionException.java
+++ InvalidInputPositionException.java
@@ -1,11 +1,7 @@
-package Homework1;
 /**
  * InvalidInputPositionException class which is invoked when user tries to implement a song
  * at a position that has its previous position unfilled.
  * @author Avish Parmar
- * SBUID: 112647892
- * Email: avish.parmar@stonybrook.edu
- * Course: CSE214
  */
 public class InvalidInputPositionException extends Exception{
 
